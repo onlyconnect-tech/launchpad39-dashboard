40368b0588259d4d39f0d5d83f1fa32f4ee27e1e
Massimo Cappellano <Massimo.Cappe@gmail.com>
2018-05-16 14:49:58 +0200

    improv

--- start_service.sh
+++ start_service.sh
@@ -11,29 +11,18 @@ cd ../launchpad39-admin-vehicles
 
 echo "starting server-admin-vehicles"
 
-process_id_admin_vehicles=`/bin/ps -fe | grep "server-admin-vehicles.js" | grep -v "grep" | awk '{print $2}'`
+process_id_admin_vehicles=`/bin/ps -fe | grep "server-admin-vehicles.js" | grep -v "grep" | grep -v "mate-terminal" | awk '{print $2}'`
 
 if [ -n "$process_id_admin_vehicles" ]; then
+    #echo `cat /proc/${process_id}/smaps`
+    # mem_res mem_shared
+    mems=(`cat /proc/${process_id_admin_vehicles}/statm | awk '{print $2 " " $3}'`) 
+    echo  "Mem Res KB ->" $((${mems[0]} * $page_size / 1024 ))
+    echo  "Mem Sha KB ->" $((${mems[1]} * $page_size / 1024 ))
     echo "server_admin_vehicles - PID:$process_id_admin_vehicles - yet running"
 else
     mate-terminal --title="ADMIN VEHICLES" -e "node server-admin-vehicles.js" &
 fi
-
-# server-permission-service.js
-
-cd ../launchpad39-permission-service
-
-echo "starting server-permission-service"
-
-process_id_permission_service=`/bin/ps -fe | grep "server-permission-service.js" | grep -v "grep" | awk '{print $2}'`
-
-if [ -n "$process_id_permission_service" ]; then
-    echo "server_permission_service - PID:$process_id_permission_service - yet running"
-else
-    mate-terminal --title="PERMISSION SERVICE" -e "node server-permission-service.js" &
-fi
-
-# server_syncronizer_DB.js
 
 cd ../launchpad39-server
 
--- stop_service.sh
+++ stop_service.sh
@@ -1,30 +1,12 @@
-process_id=`/bin/ps -fe | grep "http-server" | grep -v "grep" | grep -v "mate-terminal" | awk '{print $2}'`
-
-if [ -n "$process_id" ]; then
-    echo "KILLING http-server - PID:$process_id"
-    kill -9 $process_id
-else
-    echo "http-server not running"
-fi
+#!/bin/bash
 
 process_id_admin_vehicles=`/bin/ps -fe | grep "server-admin-vehicles.js" | grep -v "grep" | grep -v "mate-terminal" | awk '{print $2}'`
 
 if [ -n "$process_id_admin_vehicles" ]; then
     echo "KILLING server_admin_vehicles - PID:$process_id_admin_vehicles"
-    kill -9 $process_id_admin_vehicles
+    kill $process_id_admin_vehicles
 else
     echo "server_admin_vehicles not running"
-fi
-
-# server-permission-service.js
-
-process_id_permission_service=`/bin/ps -fe | grep "server-permission-service.js" | grep -v "grep" | grep -v "mate-terminal" | awk '{print $2}'`
-
-if [ -n "$process_id_permission_service" ]; then
-    echo "KILLING server_permission_service - PID:$process_id_permission_service"
-    kill -9 $process_id_permission_service
-else
-    echo "server_permission_service not running"
 fi
 
 # server_syncronizer_DB.js
@@ -33,7 +15,7 @@ process_id_synchronizer_DB=`/bin/ps -fe | grep "server_synchronizer_DB.js" | gre
 
 if [ -n "$process_id_synchronizer_DB" ]; then
     echo "KILLING server_synchronizer_DB - PID:$process_id_synchronizer_DB"
-    kill -9 $process_id_synchronizer_DB
+    kill $process_id_synchronizer_DB
 else
     echo "server_synchronizer_DB not running"
 fi
@@ -44,7 +26,7 @@ process_id_dashboard_backend=`/bin/ps -fe | grep "server_ccs_dashboard_backend.j
 
 if [ -n "$process_id_dashboard_backend" ]; then
     echo "KILLING server_dashboard_backend - PID:$process_id_dashboard_backend"
-    kill -9 $process_id_dashboard_backend
+    kill $process_id_dashboard_backend
 else
     echo "server_dashboard_backend not running"
 fi
